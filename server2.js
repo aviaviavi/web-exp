b0dfca96c78c3fdbd9ec7778a0a2c4ab23cbc235
Avi Press <avipress@gmail.com>
2012-12-31 21:28:01 -0800

    error handling, server restart, shutdown, etc

--- server2.js
+++ server2.js
@@ -9,10 +9,10 @@ var express = require('express')
     password : 'ghostdad',
   });
 
+//surpress some of the defualt logging
 io.set('log level', 1);
 
-var clientData = {};
-
+//connect to db
 connection.connect(function (err) {
   if (err) console.log(err);
 });
@@ -20,8 +20,40 @@ connection.connect(function (err) {
 //test is the db name as of now.
 connection.query('use test');
 
+//error handling, in case db disconnects
+function handleDisconnect(connection) {
+  connection.on('error', function(err) {
+    if (!err.fatal) {
+      return;
+    }
+
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      throw err;
+    }
+
+    console.log('Re-connecting lost connection: ' + err.stack);
+
+    connection = mysql.createConnection(connection.config);
+    handleDisconnect(connection);
+    connection.connect();
+  });
+}
+handleDisconnect(connection);
+
+//initiate in memory client data
+var clientData = {};
+//put all of the subjects already in the database back in clientData
+connection.query('SELECT d.subject_addr, r.response FROM data d, response r WHERE r.subject_addr = d.subject_addr', function (err, results, fields) {
+  for (i = 0; i < results.length; i++) {
+    clientData[results[i]['subject_addr']] = {'data': true};
+    clientData[results[i]['subject_addr']]['response'] = results[i]['response'];
+  }
+});
+
+//serve the files
 app.use(express.static(__dirname));
 
+//event handling
 io.sockets.on('connection', function (socket) {
 
   var address = socket.handshake.address;
@@ -31,7 +63,7 @@ io.sockets.on('connection', function (socket) {
   socket.on('simulation-data', function (data) {
     //if we haven't seen this address yet, add the data to 'data' table
     if (!clientData[address]) {
-      clientData[address] = data;
+      clientData[address] = {'data': true};
       console.log(data);
       connection.query('INSERT INTO data SET subject_addr = ?, data = ?', [address, JSON.stringify(data)], function (err, result) {
         if (err) throw err;
@@ -43,7 +75,7 @@ io.sockets.on('connection', function (socket) {
   //sent once user submits response
   socket.on('response-data', function (data) {
     //if the address exists but doesn't have a reponse, add it to the 'response table'
-  	if (clientData[address] && !clientData['response']) {
+  	if (clientData[address] && !clientData[address]['response']) {
   		clientData[address]['response'] = data['response'];
   		console.log('response added! ' + data['response']);
       connection.query('INSERT INTO response SET subject_addr = ?, response = ?', [address, data['response']], function (err, result) {
